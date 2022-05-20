0c52bf9ceba36e0fa1488ae3ec3c783705388ab1
YavuzUca <yavuz_uca@hotmail.com>
2022-05-20 13:25:44 +0200

    added a secret-example.py

---
+++ secret-example.py
@@ -0,0 +1,11 @@
+# me stands for Mobile Entry which is from the User-Agent value. The cookie and request-token speaks for itself. When
+# you're in your enviorement of BDO's marketplace, you can copy these values from the headers of the requests your
+# browser makes.
+
+me = ''
+cookie = ''
+req_token = ''
+
+# This is a list of outfits that you want to iterate through
+
+outfit_list = []
